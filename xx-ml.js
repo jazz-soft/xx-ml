3908e5578193f0967c01afa7af453433ce9d2b4e
Sema <jazz-dev@jazz-soft.net>
2025-05-18 00:00:25 -0400

    WIP

--- xx-ml.js
+++ xx-ml.js
@@ -1,12 +1,30 @@
 function XML(s) {
   this.txt = s;
-  this.eol = linebreaks(s);
+  this.eol = _eol(s);
   this.frg = [];
+  this.doc = { t: 'doc', a: [] };
+  _parse(this.doc, this.txt);
 }
 
 XML.prototype.rc = function(n) { return rc(this.eol, n); };
 
-function linebreaks(s) {
+function _parse(x, s, p) {
+  p = p || 0;
+  var n;
+  var empty = true;
+  for (n = p; n < s.length; n++) {
+    if (s[n] == '<') break;
+    if (empty && !_sp(s[n])) empty = false;
+  }
+  if (n > p) {
+    x.a.push({ t: empty ? 'space' : 'txt', p: p, len: n - p });
+  }
+  p = n;
+}
+
+function _sp(c) { return c == ' ' || c == '\t' || c == '\n'; }
+
+function _eol(s) {
   var a = [0];
   for (var n = 0; n < s.length; n++) if (s[n] == '\n') a.push(n + 1);
   return a;
